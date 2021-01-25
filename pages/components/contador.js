2d379ff4bb6c3c660216e4742592cac1f76e7916
Josimar Camilo <josimarifmg@gmail.com>
2021-01-24 21:21:09 -0300

    testando a função getStaticProps()

--- pages/components/contador.js
+++ pages/components/contador.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Contador()
+function Contador(props)
 {
     const [contador, setContador] = useState(1);
 
@@ -12,6 +12,22 @@ export default function Contador()
         <div>
             <div>{contador}</div>
             <button onClick={incrementar}>Clique aqui meu chapa</button>
+            <p>{props.staticDateString}</p>
         </div>
     );
 }
+
+export async function getStaticProps()
+{
+    console.log('> Passando pelo getStaticProps();');
+    const staticDate = new Date();
+    const staticDateString = staticDate.toGMTString();
+
+    return {
+        props: {
+            staticDateString
+        }
+    }
+}
+
+export default Contador;
