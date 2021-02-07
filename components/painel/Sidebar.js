0b20798e3454117089dd8415dd6f59d729680c4e
Josimar Camilo <josimarifmg@gmail.com>
2021-02-07 16:47:15 -0300

    Criando o Sidebar para o painel

---
+++ components/painel/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import style from './Sidebar.module.css';
+import CreditCardIcon from '@material-ui/icons/CreditCard';
+
+export default function Sidebar(){
+    return (
+        <div className ={style.estilo}>
+            <h2>Organizei.me</h2>
+            <div>
+                <div className ={style.icone}><CreditCardIcon /></div>                
+                <p>Orçamentos</p>
+            </div>
+            <div>
+                <div className ={style.icone}><CreditCardIcon /></div> 
+                <p>Receitas</p>
+            </div>
+            <div>
+                <div className ={style.icone}><CreditCardIcon /></div> 
+                <p>Despesas</p>
+            </div>
+            <div>
+                <div className ={style.icone}><CreditCardIcon /></div> 
+                <p>Objetivos financeiros</p>
+            </div>
+        </div>
+    );
+}
---
+++ pages/painel/dashboard.js
@@ -0,0 +1,6 @@
+import Sidebar from '../../components/painel/Sidebar';
+export default function Dashboard(){
+    return (
+        <Sidebar />
+    );
+}
